687d5abc30bbeeb5e71cb6aa2d2be318d338469e
JavierKDGN <javier.cadagan6@gmail.com>
2025-07-10 19:48:58 -0400

    feat: se mejoró la clase Mano y se añadieron métodos para gestionar las manos del jugador en Blackjack

--- core/player.py
+++ core/player.py
@@ -1,5 +1,6 @@
-#https://www.pokerstars.com/es-419/casino/how-to-play/blackjack/rules/?&no_redirect=1
+from .cartas import Carta, Rango, Palo
 
+#https://www.pokerstars.com/es-419/casino/how-to-play/blackjack/rules/?&no_redirect=1
 """
 cartas posibles:
     - A (1 o 11)
@@ -7,107 +8,136 @@ cartas posibles:
     - J, Q, K (10 puntos cada uno)
 """
 
-def valor_carta(carta):
-    """Devuelve el valor de una carta en el juego de Blackjack."""
-    if carta in ['J', 'Q', 'K']:
-        return 10
-    elif carta == 'A':
-        return 11  # El valor del As puede ajustarse más tarde si es necesario.
-    else:
-        return int(carta)
-
 class Mano:
-    """Clase que representa la mano de un jugador en el juego de Blackjack."""
-    def __init__(self, cartas):
-        # Inicializa la mano con una lista de cartas y una apuesta.
+    def __init__(self, cartas: list[Carta]):
+        """Clase que representa la mano de un jugador en el juego de Blackjack."""
         self.cartas = cartas
-        self.total = sum(valor_carta(carta) for carta in cartas)
-        self.ases = cartas.count('A')  # Cuenta cuántos Ases hay en la mano.
-        # Ajusta el valor del As si el total supera 21. (tocaron dos Ases)
-        if self.total > 21 and 'A' in cartas:
-            self.ases -= 1
-            self.total -= 10
         self.apuesta = 0  # Inicializa la apuesta en 0 (no apostada)
-        self.bajada = False  # Indica si la mano ya termino
+        self.turno_terminado = False  # Indica si la mano ya termino
 
-    def agregar_carta(self, carta):
-        """si se pasa, se marca la mano como bajada."""
-        self.cartas.append(carta)
-        if carta == 'A':
-            self.ases += 1
-        self.total += valor_carta(carta)
-        # Ajusta el valor del As si el total supera 21.
-        if self.total > 21:
-            if self.ases > 0:
-                self.ases -= 1
-                self.total -= 10
-            else:
-                self.bajada=True
-                return False  # La mano ha perdido, no se puede seguir jugando.
-        return True # La mano sigue vigente.
 
-    def valor_total(self):
+    @property
+    def valor_total(self) -> int:
         """Devuelve el valor total de la mano."""
-        return self.total
+        total = sum(carta.valor for carta in self.cartas)
+        ases = sum(1 for carta in self.cartas if carta.rango == Rango.AS)
 
+        while total > 21 and ases > 0:
+            total -= 10
+            ases -= 1
+
+        return total
+
+    @property
+    def es_blanda(self):
+        """
+        Indica si la mano es blanda (contiene un As que cuenta como 11).
+        """
+        total_con_as = sum(carta.valor for carta in self.cartas)
+        hay_ases = any(carta.rango == Rango.AS for carta in self.cartas)
+
+        return total_con_as <= 21 and hay_ases
+
+    def agregar_carta(self, carta: Carta):
+        """
+        Agrega una carta a la mano
+        """
+        self.cartas.append(carta)
+
+        if self.valor_total > 21:
+            self.turno_terminado = True
+
+    def __repr__(self) -> str:
+        cartas_str = ", ".join(str(c) for c in self.cartas)
+        return f"Mano(cartas=[{cartas_str}], valor = {self.valor_total}"
 
 
 class Jugador:
-    def __init__(self, nombre, capital=0):
-        # Inicializa el jugador
+    def __init__(self, nombre: str, capital: int):
+        """
+        Inicializa el jugador
+        :param nombre: Nombre del jugador
+        :param capital: Capital inicial del jugador
+        """
         self.nombre = nombre
         self.capital = capital
         self.manos = []
 
-    def apostar(self, cantidad, mano):
-        """Permite al jugador realizar una apuesta."""
-        if cantidad > self.capital:
-            return False  # No tiene suficiente capital para apostar.
+    def reset_manos(self):
+        """
+        Reinicia la mano del jugador para una nueva ronda.
+        """
+        self.manos = []
+
+    def apostar(self, cantidad: int) -> bool:
+        """
+        Permite al jugador realizar una apuesta inicial
+        :param cantidad: Cantidad a apostar
+        :return: True si la apuesta se ha realizado correctamente, False en caso contrario.
+        """
+
+        # No tiene suficiente capital para apostar.
+        if cantidad <= 0 or cantidad > self.capital:
+            return False
+
+        self.capital -= cantidad
+        mano = Mano([])
         mano.apuesta = cantidad
         self.manos.append(mano)
-        self.capital -= cantidad
-
-    def pedir_carta(self, mano, carta):
-        """Permite al jugador pedir una carta adicional."""
-        if mano in self.manos: 
-            return mano.agregar_carta(carta) # La mano ha perdido, no se puede seguir jugando.
-
-            
-    def terminar_mano(self, mano):
-        """marca la mano como terminada."""
-        if mano in self.manos:
-            mano.bajada = True  # Marca la mano como terminada.
-
-    def split(self, mano):
-        """Permite al jugador dividir su mano si tiene dos cartas del mismo valor."""
-        if len(mano.cartas) == 2 and valor_carta(mano.cartas[0]) == valor_carta(mano.cartas[1]) and self.capital >= mano.apuesta:
-            nueva_mano = Mano([mano.cartas.pop()])
-            self.capital -= mano.apuesta
-            nueva_mano.apuesta = mano.apuesta
-            self.manos.append(nueva_mano)
-            return True  # Se ha realizado el split.
-        return False  # No se puede realizar el split.
-            
-
-    def doblar(self, mano, carta):
-        """Permite al jugador doblar su apuesta y recibir una carta adicional."""
-        if self.capital < mano.apuesta or len(mano.cartas) != 2:
-            return False  # No puede doblar si no tiene suficiente capital o si ya tiene más de 2 cartas.
-        self.capital -= mano.apuesta
-        mano.apuesta *= 2
-        self.pedir_carta(mano, carta)  # Pide una carta adicional.
-        self.terminar_mano(mano)  # Termina la mano después de doblar.
         return True
 
 
-    def surrender(self, mano):
-        """Permite al jugador rendirse y recuperar la mitad de su apuesta."""
+    def pedir_carta(self, mano: Mano, carta: Carta):
+        """
+        Agrega una carta a la mano del jugador.
+        """
         if mano in self.manos:
-            self.capital += mano.apuesta // 2  # Asegura que sea un entero
+            return mano.agregar_carta(carta) # La mano ha perdido, no se puede seguir jugando.
+        return None
+
+
+    def dividir_mano(self, mano: Mano) -> bool:
+        """
+        Permite al jugador dividir su mano si tiene dos cartas del mismo valor.
+        :param mano: Mano a dividir
+        :return: True si se ha realizado el split, False en caso contrario.
+        """
+        if len(mano.cartas) != 2 and self.capital >= mano.apuesta:
+            return False
+
+        if mano.cartas[0].valor != mano.cartas[1].valor:
+            # No se puede dividir si las cartas no son del mismo valor.
+            return False
+
+        self.capital -= mano.apuesta
+        carta_dividida = mano.cartas.pop()
+        nueva_mano = Mano([carta_dividida])
+        nueva_mano.apuesta = mano.apuesta
+        self.manos.append(nueva_mano)
+        return True  # Se ha realizado el split.
+
+
+    def doblar_apuesta(self, mano: Mano, carta: Carta):
+        """
+        Permite al jugador doblar su apuesta.
+        El casino se encarga de repartir una carta adicional.
+        """
+        if self.capital < mano.apuesta:
+            # No puede doblar si no tiene suficiente capital o si ya tiene más de 2 cartas.
+            return False
+
+        self.capital -= mano.apuesta
+        mano.apuesta *= 2
+        return True
+
+    def rendirse(self, mano: Mano):
+        """
+        Permite al jugador rendirse y recuperar la mitad de su apuesta.
+        """
+        if mano in self.manos:
+            # Asegura que sea un entero
+            self.capital += mano.apuesta // 2
             self.manos.remove(mano)
             return True
         return False  # La mano no existe, no se puede rendir.
 
-    def reset_manos(self):
-        """Reinicia la mano del jugador para una nueva ronda."""
-        self.manos = []

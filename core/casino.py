1c0870a63711cb65b2fa224a1ac2dfca372869ea
JavierKDGN <javier.cadagan6@gmail.com>
2025-07-10 23:13:21 -0400

    feat: refactor Casino para el nuevo formato de cartas

--- core/casino.py
+++ core/casino.py
@@ -1,167 +1,222 @@
 from random import shuffle
-from player import Jugador, Mano
+
+from .acciones import Accion
+from .player import Jugador, Mano
+from .cartas import Mazo, Carta
+from agents.agente_base import Agente
 
 
 class Casino:
-    def __init__(self, agentes, dealer_nombre="Dealer", max_stands=3, min_apuesta=5000):
-        """Inicializa el juego con una lista de jugadores y un dealer."""
+    def __init__(self, agentes: list[Agente], num_mazos: int = 4, zapato: float = 0.75):
         self.agentes = agentes
-        self.max_stands = max_stands
-        # un jugador no puede plantarse mas de "max_stands" veces
-        self.dealer = Jugador(dealer_nombre) # Dealer
-        self.baraja = []  # Baraja de cartas
-        self.min_apuesta = min_apuesta  # Apuesta mínima para cada jugador
+        self.num_mazos = num_mazos
+        self.zapato = zapato
+        self.dealer = Jugador(nombre="Dealer", capital=10000)
+        self.mazo = Mazo(num_mazos=num_mazos, zapato=zapato)
 
-    def reset_baraja(self):
-        """Crea y mezcla una baraja estándar de Blackjack."""
-        baraja = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'] * 4
-        shuffle(baraja)
-        self.baraja = baraja
-
-    def repartir_cartas(self):
-        """
-        Reparte dos cartas al dealer y luego a cada jugador.
-        Cada jugador recibe dos cartas y el dealer recibe una carta visible y una oculta.
-        También establece la apuesta inicial de cada jugador.
-        """
-        # Reparte dos cartas al dealer
-        self.dealer.apostar(0, Mano([self.baraja.pop(), self.baraja.pop()]))
-        # Reparte dos cartas a cada jugador
+    def _notificar_observadores(self, carta: Carta):
         for agente in self.agentes:
-            if agente.banca_rota:
-                print(f"{agente.jugador.nombre} no puede jugar, la banca está rota.")
-                agente.termino = True
-                self.fin_ronda_per_mano -= 1
-            else:
-                # Establece la apuesta inicial del jugador
-                apuesta = agente.apostar()
-                if apuesta > self.min_apuesta:
-                    agente.jugador.apostar(apuesta, Mano([self.baraja.pop(), self.baraja.pop()]))
-                else:
-                    print(f"{agente.jugador.nombre} no tiene suficiente capital para apostar. Apuesta mínima: {self.min_apuesta}")
-                    agente.termino = True  # Marca al jugador como que no puede jugar
-                    self.fin_ronda_per_mano -= 1
+                # Solo los que tienen implementado observar haran algo
+                agente.observar_carta(carta)
 
-    def first_turn(self, agente):
-        """
-        Gestiona el primer turno de los agentes.
-        solo se permite: split y doblar
-        """
-        jugada=agente.decidir_accion_primer_turno()
-        if jugada == 'split':
-            if agente.jugador.split(agente.jugador.manos[0]):
-                carta = self.baraja.pop()
-                agente.jugador.pedir_carta(agente.jugador.manos[0], carta)
-                carta = self.baraja.pop()
-                agente.jugador.pedir_carta(agente.jugador.manos[1], carta)
-        elif jugada == 'doblar':
-            carta = self.baraja.pop()
-            if agente.jugador.doblar(agente.jugador.manos[0], carta):
-                agente.termino = True  # Marca al jugador que ya termino de jugar
-            else:                
-                print(f"{agente.jugador.nombre} no puede doblar, no tiene capital suficiente.")
-                self.baraja.append(carta)  # Devuelve la carta a la baraja
-        elif jugada == 'nada':
-            pass
-        else:
-            print(f"Acción no válida: {jugada}")
+    def _jugar_ronda(self):
 
+        #1. Fase de Preparacion
 
-    def turno_jugador(self, agente):
-        """
-        Gestiona los turnos estandar de un agente
-        solo se permite: terminar_mano, stand, pedir_carta y surrender
-        jugadas contiene [tipo_jugada, mano]
-        """
-        jugadas = agente.decidir_accion()
-        for jugada in jugadas:
-            tipo_jugada, mano = jugada
-            if tipo_jugada == 'terminar_mano':
-                agente.jugador.terminar_mano(mano)
-            elif tipo_jugada == 'stand':
-                if agente.veces_plantado >= self.max_stands:
-                    print(f"{agente.jugador.nombre} ha alcanzado el máximo de plantadas.")
-                    agente.jugador.terminar_mano(mano)
-                else:
-                    agente.veces_plantado += 1
-            elif tipo_jugada == 'pedir_carta':
-                carta = self.baraja.pop()
-                agente.jugador.pedir_carta(mano, carta)
-            elif tipo_jugada == 'surrender':
-                agente.jugador.surrender(mano)
+        # Barajar el mazo si es necesario (Crearlo de nuevo)
+        if self.mazo.necesita_barajar():
+            self.mazo = Mazo(num_mazos=self.num_mazos, zapato=self.zapato)
 
-        # Check if all hands of the agent are finished
-        if not agente.jugador.manos or all(m.bajada for m in agente.jugador.manos):
-            agente.termino = True
-
-    def turno_dealer(self):
-        """Gestiona el turno del dealer."""
-        while self.dealer.manos[0].valor_total() < 17:
-            self.dealer.manos[0].agregar_carta(self.baraja.pop())
-
-    def determinar_ganadores(self):
-        """
-        Determina los ganadores de la ronda y les entrega las ganancias.
-        solo se necesita revisar si la mano gana al dealer.
-        """
-        if self.dealer.manos[0].valor_total() > 21:
-            print(f"El dealer ha perdido con la mano {self.dealer.manos[0].cartas}.")
-            for agente in self.agentes:
-                for mano in agente.jugador.manos:
-                    if mano.valor_total() <= 21:
-                        print(f"{agente.jugador.nombre} ha ganado con la mano {mano.cartas}.")
-                        agente.jugador.capital += mano.apuesta * 2
-                    else: #empate
-                        print(f"{agente.jugador.nombre} ha empatado con la mano {mano.cartas}.")
-                        agente.jugador.capital += mano.apuesta
-        else:
-            for agente in self.agentes:
-                for mano in agente.jugador.manos:
-                    if mano.valor_total() > self.dealer.manos[0].valor_total():
-                        if mano.valor_total() <= 21:
-                            print(f"{agente.jugador.nombre} ha ganado con la mano {mano.cartas}.")
-                            agente.jugador.capital += mano.apuesta * 2
-                        else:
-                            print(f"{agente.jugador.nombre} ha perdido con la mano {mano.cartas}.")
-                    elif mano.valor_total() == self.dealer.manos[0].valor_total():
-                        print(f"{agente.jugador.nombre} ha empatado con la mano {mano.cartas}.")
-                        agente.jugador.capital += mano.apuesta
-                    else:
-                        print(f"{agente.jugador.nombre} ha perdido con la mano {mano.cartas}.")
-
-
-    def store_data_per_round(self):
-        """Guarda los datos de la ronda en un archivo CSV."""
-        pass
-
-    def store_data_per_game(self):
-        """Guarda los datos del juego en un archivo CSV."""
-        pass
-
-    def jugar_ronda(self):
-        """Juega una ronda completa de Blackjack."""
-        print("Iniciando una nueva ronda...")
-        self.reset_baraja()
-        self.repartir_cartas()
         for agente in self.agentes:
-            self.first_turn(agente)
-
-        while True:
-            for agente in self.agentes:
-                if not agente.termino:
-                    self.turno_jugador(agente)
-            # Ensure the loop exits if all agents are done
-            if all(agente.termino for agente in self.agentes):
-                break
-
-        self.turno_dealer()
-        self.determinar_ganadores()
-
-        # Reinicia las variables para la próxima ronda
-        for agente in self.agentes:
-            agente.veces_plantado = 0
-            agente.termino = False
             agente.jugador.reset_manos()
         self.dealer.reset_manos()
 
-        print("Ronda finalizada.")
+        #2. Fase de Apuestas
+
+        # Los agentes que si tienen para apostar se agregan a la lista
+        agentes_activos = []
+        for agente in self.agentes:
+            apuesta = agente.decidir_apuesta()
+            if agente.jugador.apostar(apuesta):
+                agentes_activos.append(agente)
+
+        #3. Fase de Reparto
+
+        # Crear mano del dealer
+        self.dealer.manos.append(Mano([]))
+        self.carta_oculta_dealer = None
+
+        # Repartir primera carta a todos
+        for agente in agentes_activos:
+            carta = self.mazo.repartir()
+            agente.jugador.pedir_carta(agente.jugador.manos[0], carta)
+            # Notificar para agentes que cuentan cartas
+            self._notificar_observadores(carta)
+
+        carta_visible_dealer = self.mazo.repartir()
+        self.dealer.pedir_carta(self.dealer.manos[0], carta_visible_dealer)
+        self._notificar_observadores(carta_visible_dealer)
+
+        # Repartir segunda carta a todos
+        for agente in agentes_activos:
+            carta = self.mazo.repartir()
+            agente.jugador.pedir_carta(agente.jugador.manos[0], carta)
+            # Notificar para agentes que cuentan cartas
+            self._notificar_observadores(carta)
+
+        # Repartimos la carta al dealer que no se muestra
+        self.carta_oculta_dealer = self.mazo.repartir()
+
+        # 4. Fase de Jugadores
+
+        for agente in agentes_activos:
+            indice_mano_actual = 0
+            while indice_mano_actual < len(agente.jugador.manos):
+
+                mano_actual = agente.jugador.manos[indice_mano_actual]
+
+                # Ciclo de juego para mano actual
+                while not mano_actual.turno_terminado:
+                    # Revisar carta del dealer
+                    carta_visible_dealer = self.dealer.manos[0].cartas[0]
+
+                    # Pedir decision al agente
+                    accion = agente.decidir_accion(mano_actual, carta_visible_dealer)
+
+                    # El casino valida el movimiento
+
+                    if accion == Accion.PLANTARSE:
+                        mano_actual.turno_terminado = True
+
+                    elif accion == Accion.PEDIR:
+                        carta_nueva = self.mazo.repartir()
+                        agente.jugador.pedir_carta(mano_actual, carta_nueva)
+                        self._notificar_observadores(carta_nueva)
+                        # Si se pasa de 21 se termina, logica en Mano.agregar_carta
+
+                    elif accion == Accion.DOBLAR:
+                        if len(mano_actual.cartas) == 2:
+                            if agente.jugador.doblar_apuesta(mano_actual):
+                                # Si se pudo, repartir y terminar turno
+                                carta_nueva = self.mazo.repartir()
+                                agente.jugador.pedir_carta(mano_actual, carta_nueva)
+                                self._notificar_observadores(carta_nueva)
+                                mano_actual.turno_terminado = True
+                            else:
+                                # No le alcanzaba capital, se planta
+                                print("No se pudo doblar, plantando")
+                                mano_actual.turno_terminado = True
+                        else:
+                            print("No se puede doblar con mas de 2 cartas, plantando")
+                            mano_actual.turno_terminado = True
+
+                    elif accion == Accion.DIVIDIR:
+                        # Validar si hay 2 cartas
+                        if len(mano_actual.cartas) == 2:
+                            if agente.jugador.dividir_mano(mano_actual):
+                                # Si fue exitoso, repartir una carta nueva a cada mano
+
+                                # Carta mano original
+                                carta1 = self.mazo.repartir()
+                                agente.jugador.pedir_carta(mano_actual, carta1)
+                                self._notificar_observadores(carta1)
+
+                                # Carta mano nueva (ultima en la lista)
+                                mano_nueva = agente.jugador.manos[-1]
+                                carta2 = self.mazo.repartir()
+                                agente.jugador.pedir_carta(mano_nueva, carta2)
+                                self._notificar_observadores(carta2)
+
+                                # Aca sigue el bucle whie, no termina el turno
+                                # Se jugara la primera mano y luego la mano nueva
+                                # se jugara cuando llegue a su indice
+                            else:
+                                print("No se pudo dividir, plantando")
+                                mano_actual.turno_terminado = True
+                        else:
+                            print("No se puede dividir con mas de 2 cartas, plantando")
+
+                    elif accion == Accion.RENDIRSE:
+                        agente.jugador.rendirse(mano_actual)
+                        break
+
+                indice_mano_actual += 1
+
+        # 5. Fase de Dealer
+        # Agregamos la carta oculta a la mano del dealer
+        self.dealer.pedir_carta(self.dealer.manos[0], self.carta_oculta_dealer)
+        self._notificar_observadores(self.carta_oculta_dealer)
+
+        # El dealer pide mientras tenga menos que 17
+        # Se planta con un 17 blando (A,6), valor_total maneja esto
+        while self.dealer.manos[0].valor_total < 17:
+            carta_nueva_dealer = self.mazo.repartir()
+            self.dealer.pedir_carta(self.dealer.manos[0], carta_nueva_dealer)
+            self._notificar_observadores(carta_nueva_dealer)
+
+
+        # 6. Fase de pagos
+
+        valor_final_dealer = self.dealer.manos[0].valor_total
+        dealer_se_paso = valor_final_dealer > 21
+        dealer_tiene_bj = self.dealer.manos[0].es_blackjack
+
+        for agente in agentes_activos:
+            for mano in agente.jugador.manos:
+
+                if mano.es_blackjack:
+                    if not dealer_tiene_bj:
+                        # Jugador gana 3:2
+                        ganancia = int(mano.apuesta * 1.5)
+                        agente.jugador.capital += mano.apuesta + ganancia
+                        print(f"{agente.jugador.nombre} gana con {mano.cartas} (blackjack)")
+                    else:
+                        # Empate, ambos tienen bj
+                        agente.jugador.capital += mano.apuesta
+                        print(f"{agente.jugador.nombre} empata con {mano.cartas}")
+                    continue
+
+                valor_mano_jugador = mano.valor_total
+                jugador_se_paso = valor_mano_jugador > 21
+
+                if jugador_se_paso:
+                    # Si se paso de 21
+                    print(f"{agente.jugador.nombre} pierde con {mano.cartas} (se paso)")
+                    pass
+
+                elif dealer_se_paso:
+                    # Jugador gana, dealer se paso
+                    # Pago es 1:1, (si jugador pago 10, recupera apuesta y gana (recibe 20))
+                    agente.jugador.capital += mano.apuesta * 2
+                    print(f"{agente.jugador.nombre} gana con {mano.cartas} (dealer se paso)")
+
+                elif valor_mano_jugador > valor_final_dealer:
+                    # Si no se paso de 21, y tiene mayor valor que el dealer
+                    agente.jugador.capital += mano.apuesta * 2
+                    print(f"{agente.jugador.nombre} gana con {mano.cartas}")
+
+                elif valor_mano_jugador == valor_final_dealer:
+                    # Empate, el jugador recupera apuesta
+                    agente.jugador.capital += mano.apuesta
+                    print(f"{agente.jugador.nombre} empata con {mano.cartas}")
+
+
+                else:
+                    # valor mano jugador < valor mano dealer
+                    print(f"{agente.jugador.nombre} pierde con {mano.cartas}")
+                    pass
+
+
+
+    def jugar_partida(self, num_rondas:int):
+        """
+        Punto de entrada para iniciar la simulacion
+        """
+        print(f"Iniciando partida de {num_rondas} rondas")
+        for i in range(num_rondas):
+            print(f"Ronda {i + 1} / {num_rondas}")
+            self._jugar_ronda()
+            for agente in self.agentes:
+                print(f"{agente.jugador.nombre}: Capital = {agente.jugador.capital}")
+        print("Partida terminada")
+

0f875642ca8683c118e014db0931dd397d823773
JavierKDGN <javier.cadagan6@gmail.com>
2025-07-10 20:20:30 -0400

    feat: Agente abstracto listo y AgenteAleatorio adaptado

--- agents/agente_base.py
+++ agents/agente_base.py
@@ -1,32 +1,34 @@
-class Agente:
-    def __init__(self, Jugador):
-        """Inicializa el agente con un jugador."""
-        self.jugador = Jugador
-        self.veces_plantado = 0  # Contador de veces que el jugador se ha plantado en la ronda
-        self.termino = False  # Indica si ya ha terminado todas las manos
-        self.banca_rota = False  # Indica si la banca está rota
+from abc import ABC, abstractmethod
+from ..core.player import Jugador, Mano
+from ..core.cartas import Carta
+from ..core.acciones import Accion
 
-    def apostar(self):
+class Agente(ABC):
+    def __init__(self, jugador: Jugador):
         """
-        Método elige cuanto apostar.
-        Este método debe ser implementado por las subclases.
+        Inicializa el agente con un jugador.
         """
-        raise NotImplementedError("Este método debe ser implementado por una subclase.")
-    
-    def decidir_accion_primer_turno(self):
-        """
-        Método que decide la acción a tomar en el primer turno.
-        Este método debe ser implementado por las subclases.
-        solo se permite: split y doblar.
-        devuelve una jugada por mano.
-        """
-        raise NotImplementedError("Este método debe ser implementado por una subclase.")
 
-    def decidir_accion(self):
+        self.jugador = jugador
+
+
+    @abstractmethod
+    def decidir_apuesta(self) -> int:
         """
-        Método que decide la acción a tomar en el turno estándar.
-        Este método debe ser implementado por las subclases.
-        solo se permite: terminar_mano, stand, pedir_carta y surrender
-        jugadas contiene [tipo_jugada, mano]
+        Elige cuanto apostar al inicio de una ronda.
         """
-        raise NotImplementedError("Este método debe ser implementado por una subclase.")	
+
+    @abstractmethod
+    def decidir_accion(self, mano: Mano, carta_dealer: Carta) -> Accion:
+        """
+        Decide la jugada a realizar para una mano especifica.
+        :param mano: Mano del jugador:
+        :param carta_dealer: Carta visible del dealer.
+        :return: Accion del Enum Accion.
+        """
+
+    def observar_carta(self, carta: Carta):
+        """
+        Para agentes contadores de cartas, el resto lo puede ignorar.
+        """
+        pass
